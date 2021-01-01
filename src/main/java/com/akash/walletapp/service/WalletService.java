68ec9b3a860b9fdb2b91e43dec3a4b7a1d5c8992
AkashBhuiyan <akash.bhuiyan@gmail.com>
2021-01-02 01:23:46 +0600

    Custom exceptions added for wallet and Delete wallet api added

--- src/main/java/com/akash/walletapp/service/WalletService.java
+++ src/main/java/com/akash/walletapp/service/WalletService.java
@@ -1,11 +1,13 @@
 package com.akash.walletapp.service;
 
 import com.akash.walletapp.entity.Wallet;
+import com.akash.walletapp.exception.WalletException;
 import com.akash.walletapp.repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WalletService {
@@ -20,5 +22,16 @@ public class WalletService {
             walletRepository.save(wallet);
         }
         return wallet;
+    }
+
+    public boolean delete(Long id){
+
+        Optional<Wallet> wallet = walletRepository.findById(id);
+
+        if(wallet.isPresent()){
+            walletRepository.delete(wallet.get());
+            return true;
+        }
+        throw new WalletException("Wallet with "+ id+ " does not exist!");
     }
 }
